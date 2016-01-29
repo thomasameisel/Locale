a5613e52f7cf2d606eca9a506883d8cce860482c
Collin Jackson <collinjackson93@gmail.com>
2016-01-29 16:45:05 -0600

    Started adding tests (didn't get too far) and added more code to pull Chicago Tribune data

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var should = chai.should();
+
+describe('TribuneData', function() {
+  var tribune = require('../lib/tribuneData');
+
+  describe('Crime', function() {
+    it()
+  });
+});
