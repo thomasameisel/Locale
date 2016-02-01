3c3d4e9768a8c6b8438c1a392cf73cd7538fb70d
collinjackson93 <collinjackson93@gmail.com>
2016-01-31 18:46:57 -0600

    Working on tests still

--- test/test.js
+++ test/test.js
@@ -4,7 +4,18 @@ var should = chai.should();
 describe('TribuneData', function() {
   var tribune = require('../lib/tribuneData');
 
-  describe('Crime', function() {
-    it()
+  describe('getYearCrimeCount', function() {
+    it('should return a number', function(done) {
+      tribune.getYearCrimeCount(1, 2016, function(err, num) {
+        if(err) {
+          done(err);
+        } else {
+          num.should.be.a('number');
+          done();
+        }
+      });
+    });
+
+
   });
 });
