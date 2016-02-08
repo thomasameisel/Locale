4414ce33203308e1d4a4d75dd401a54eac2bcecd
qc12su <qc12su@gmail.com>
2016-02-08 12:25:18 -0600

    Added markers for map

--- public/views/map/mapController.js
+++ public/views/map/mapController.js
@@ -1,7 +1,23 @@
 app.controller('mapController', function($scope, $location) {
-    var s = document.getElementById('map');
     var map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 41.8369, lng: -87.6847},
+            center: {lat: 41.881832, lng: -87.623177},
             zoom: 12
         });
+
+    var coordinates = [
+            {lat: 41.881832, lng: -87.623177},
+            {lat: 41.891832, lng: -87.6545},
+            {lat: 41.911832, lng: -87.7237},
+            {lat: 41.821832, lng: -87.647}
+        ];
+
+    $scope.markers = [];
+
+    coordinates.forEach(function (location) {
+        $scope.markers.push(new google.maps.Marker({
+            position: location,
+            map: map,
+            title: 'Apartment'
+        }));
+    });
 });
