a6f8edfbd0f7c92c5e002116ad318f78b2cb47be
Collin Jackson <collinjackson93@gmail.com>
2016-02-15 16:59:58 -0600

    Tommy tried to fix his bad code, but didn't finish.

--- server.js
+++ server.js
@@ -10,42 +10,55 @@ var app = express();
 
 app.use(express.static('public'));
 
-function validOps(ops) {
-  for (var op in ops) {
-    if (ops[op] !== '<' && ops[op] !== '>') {
-      return false;
-    }
-  }
-  return true;
+function validOp(op) {
+  return op === '<' || op === '>';
 }
 
-function validNums(nums) {
-  for (var num in nums) {
-    if (!validator.isInt(nums[num]) || num < 1 || num > 5) {
-      return false;
-    }
+function validNum(num) {
+  return validator.isInt(num) && num > 0 && num < 6;
+}
+
+function validParam(param) {
+  var valid = {
+    violentCrime: true,
+    nonViolentCrime: true,
+    nightlife: true,
+    price: true,
+    crowded: true
+  };
+  if (valid[param]) {
+    return true;
+  } else {
+    return false;
   }
-  return true;
 }
 
 function validParams(opsAndNums) {
-  return opsAndNums.ops && opsAndNums.nums && validOps(opsAndNums.ops) &&
-      validNums(opsAndNums.nums);
+  for (var preference in opsAndNums) {
+    if (opsAndNums.hasOwnProperty(preference) &&
+          (!validOp(opsAndNums[preference].op) ||
+          !validNum(opsAndNums[preference].num) ||
+          !validParam(preference))) {
+      return false;
+    }
+  }
+  return true;
 }
 
 // jscs:disable
-// http://localhost:8080/preferences?ops[violentCrime]=%3C&ops[nonViolentCrime]=%3C&ops[nightlife]=%3E&ops[price]=%3C&ops[crowded]=%3C&nums[violentCrime]=4&nums[nonViolentCrime]=3&nums[nightlife]=4&nums[price]=4&nums[crowded]=2
+// http://localhost:8080/preferences?violentCrime[op]=%3C&nonViolentCrime[op]=%3C&nightlife[op]=%3E&price[op]=%3C&crowded[op]=%3C&violentCrime[num]=4&nonViolentCrime[num]=3&nightlife[num]=4&price[num]=4&crowded[num]=2
 // jscs:enable
 app.get('/preferences', function(req, res) {
+  console.log(req.query);
   if (validParams(req.query)) {
     var params = {
-      violentCrimePctOfAvg: [req.query.ops.violentCrime,
-                              req.query.nums.violentCrime],
-      nonViolentCrimePctOfAvg: [req.query.ops.nonViolentCrime,
-                                req.query.nums.nonViolentCrime],
-      nightlifePctOfAvg: [req.query.ops.nightlife, req.query.nums.nightlife],
-      pricePctOfAvg: [req.query.ops.price, req.query.nums.price],
-      crowdedPctOfAvg: [req.query.ops.crowded, req.query.nums.crowded]
+      violentCrimePctOfAvg: [req.query.violentCrime.op,
+                              req.query.violentCrime.num],
+      nonViolentCrimePctOfAvg: [req.query.nonViolentCrime.op,
+                                req.query.nonViolentCrime.num],
+      nightlifePctOfAvg: [req.query.nightlife.op, req.query.nightlife.num],
+      pricePctOfAvg: [req.query.price.op, req.query.price.num],
+      crowdedPctOfAvg: [req.query.crowded.op, req.query.crowded.num]
     };
     preferencesCommunities.communitiesByPreferences(params,
                                                     function(err, result) {
@@ -64,6 +77,7 @@ app.get('/preferences', function(req, res) {
 // http://localhost:8080/directions?destination=201%20S%20Wacker%20Dr,%20Chicago,%20IL&mode=driving
 // jscs:enable
 app.get('/directions', function(req, res) {
+  console.log(req.query);
   if (req.query.destination && req.query.mode &&
       (req.query.mode === 'driving' || req.query.mode === 'transit')) {
     var params = {
