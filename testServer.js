f643066fbdbdca973e6da0bce57709f5dde76da0
Collin Jackson <collinjackson93@gmail.com>
2016-03-22 20:30:28 -0500

    Added ability to visualize directions coords in the database.

--- testServer.js
+++ testServer.js
@@ -5,9 +5,14 @@ app.set('views', __dirname);
 app.set('view engine', 'jade');
 
 var pointString = fs.readFileSync('./lib/unusedPoints.txt');
+var directionsString = fs.readFileSync('./lib/directionsCoords.json');
 
 app.get('/', function(req, res) {
-  res.render('index.jade', { points: pointString});
+  res.render('index.jade', { points: pointString });
+});
+
+app.get('/directions', function(req, res) {
+  res.render('index.jade', { points: directionsString });
 });
 
 app.listen(8080, function() {
